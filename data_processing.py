ed2c341fa221c0b5a5319e2b391aed5b365bd21f
Joseph King <jking@luxoft.com>
2025-03-18 12:13:12 +0100

    changes that pertain to initial data processing, adapting to new xgboost logic. Also some general improvements

--- data_processing.py
+++ data_processing.py
@@ -9,89 +9,98 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from ImbalancedLearningRegression import smote
+import xgboost
 
-from utils import set_seeds  # needed for reproducibility if using SMOTE
+from utils import set_seeds, is_bool_or_binary
 set_seeds(42)
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)  # 
+warnings.filterwarnings("ignore", category=DeprecationWarning)  #
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
 class DataProcessor:
-    
-    def __init__(self, 
+
+    def __init__(self,
                  unit_identifier: str,
                  time_identifier: str,
                  target_feature: str,
+                 categorical_features_prefix: str,
                  test_size: float = 0.1,
                  seed: int = 42,
-                 model: str = 'svm',
-                 lag_length: int = 5,
-                 sampling_n: int = 10,
+                 lag_length: int = 2,
+                 sampling_n: int = 5,
                  oversample: bool = False
                  ):
-        
+
         self.unit_identifier = unit_identifier
         self.time_identifier = time_identifier
         self.target_feature = target_feature
+        self.categorical_features_prefix = categorical_features_prefix
         self.test_size = test_size
         self.seed = seed
-        self.model = model
-        self.lag_length = 0 if sampling_n == 1 else lag_length  # lags not possible with sampling_n=1
-        self.sampling_n = max(1, sampling_n)  # minimum 1
+        self.sampling_n = max(1, sampling_n)  # minimum 1, negative values not possible
         self.oversample = oversample
-        
-    def preprocess(self, df: pd.DataFrame) -> pd.DataFrame:
+        assert lag_length < sampling_n, "Error! `lag_length` must be less than `sampling_n`!"
+        self.lag_length = min(lag_length, 5) # maximum 5 lags, since higher number increases chance of overfitting
+
+
+    def preprocess(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, np.ndarray, np.ndarray]:
         """
-        Initial preprocessing of data including: 
+        Initial preprocessing of data including:
             1) change target to bool
             2) Downsample the panel to reduce total observations and the influence of outliers. Done with stratified random sampling technique, where `self.time_identifier` used to stratify observations per `self.unit_identifier`
             3) addition of lagged features for each `self.unit_identifier`
-            4) creation of final outcome features for scikit-survival model, based the last timepoint per `self.unit_identifier`
+            4) creation of final outcome features for xgboost interval censoring
         :param df: pd.DataFrame, input dataframe
-        :return: pd.DataFrame
+        :return: Tuple of [pd.DataFrame, np.ndarray, np.ndarray]
         """
         current = time()
         logging.info("Preprocessing data..")
-        
+
         # Recode label to bool -> whether event observed or censored. Needed for classical survival analysis
         # Note: `self.target_feature`=True should mean the outcome is observed, =False is censored
-        # Note: scikit-survival expect this as bool - will need to modify if use other 3rd party module
-        assert df[self.target_feature].nunique() == 2, f"Error! Maximum number of allowed categories in target feature is 2! However {df[self.target_feature].nunique()} were found!"
-        assert df[self.target_feature].min() == 0, "Error! Expected target feature to be coded {0, 1}"
-        assert df[self.target_feature].max() == 1, "Error! Expected target feature to be coded {0, 1}"
-        df[self.target_feature] = np.where(df[self.target_feature] == 1, True, False)
-        
-        last_cycle = df[df[self.time_identifier] == df.groupby(self.unit_identifier)[self.time_identifier].transform('max')][
-            [self.unit_identifier, self.target_feature, self.time_identifier]].set_index(self.unit_identifier)
-        
+        assert df[self.target_feature].apply(is_bool_or_binary).all(), "Error! `self.target_feature` must be boolean or binary (0, 1)!"
+
+        # Verify `self.df` is in fact a panel dataset
+        max_group_obs = df.groupby(self.unit_identifier)[self.time_identifier].count().max()
+        if max_group_obs == 1 & self.sampling_n > 1:
+            logging.warning(f"`self.df` does not appear to be a panel dataset, even though `self.sampling_n` > 1. Setting `self.sampling_n` to 1..")
+            self.sampling_n = 1
+            self.lag_length = 0  # no lags for cross-sectional data
+
+        # Downsample panel
         if self.sampling_n > 1:
-            # Downsample panel
             logging.info(f"Downsampling panel to min({self.sampling_n}, len(x)) observations per '{self.unit_identifier}'..")
             df = self.sample_group_observations(df)
         else:  # keep last observation per unit
-            logging.info(f"Keeping only last observation per '{self.unit_identifier}'..")
+            logging.info(f"Keeping only last observation per '{self.unit_identifier}'. This will not affect purely cross-sectional data.")
             df = df[df[self.time_identifier] == df.groupby(self.unit_identifier)[self.time_identifier].transform('max')]
 
         df = df.set_index(self.unit_identifier)
-        
-        # Add feature lags [optional], else renames columns to `{colname}_l0`
+
+        # Set categorical features
+        cats = [i for i in df.columns if self.categorical_features_prefix in i]
+        df[cats] = df[cats].astype('category')
+
+        # Create label bounds for xgboost interval censoring
+        if self.sampling_n == 0:  # simple cross-sectional data
+            y_lower_bound = df[self.time_identifier].copy()
+            y_upper_bound = np.where(df[self.target_feature] == 1, df[self.time_identifier], +np.inf)
+        else:  # panel data
+            y_lower_bound = np.array(df.groupby(self.unit_identifier)[self.time_identifier].shift(1).fillna(0))
+            y_upper_bound = np.where(df[self.target_feature] == 1, df[self.time_identifier], +np.inf)
+        df = df.drop(columns=[self.time_identifier, self.target_feature])  # drop target feature since it's already in `y_lower_bound` and `y_upper_bound`
+
+        # Add feature lags [optional]
         if self.lag_length > 0:
             df = self.feature_lags(df)
-        else:
-            df = df.drop(columns=[self.target_feature])  # drop target feature, since it's already in `last_cycle`
-            cols = [i for i in df.columns if "Cat" not in i]  # note - "Cat" is dataset specific, remove if not needed
-            df = df.rename(columns={col: f"{col}_l0" for col in cols})  # rename contemporary features to `_l0` for consistency, note excludes invariant "Cat" cols
 
-        # Concat `self.target_feature` so that this column is the same for all `self.time_identifier` (time periods) for a given `self.unit_identifier`
-        df = df.merge(last_cycle, left_index=True, right_index=True)
-        
         logging.info(f"Data preprocessing complete, took: {round(time() - current, 2)} seconds. \n")
 
-        return df.reset_index()
-    
+        return (df.reset_index(), y_lower_bound, y_upper_bound)
+
 
     def split_transform(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, np.ndarray, np.ndarray]:
         """
@@ -103,7 +112,7 @@ class DataProcessor:
         """
         current = time()
         logging.info("Splitting & transforming data..")
-        
+
         # Stratified train-test-val-split
         X_train, X_test, y_train, y_test = self.stratified_train_test_val_split(df)  # removes self.unit_identifier from index
         
@@ -166,22 +175,19 @@ class DataProcessor:
 
     def feature_lags(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        Iterates over predictor columns in `df` pivoting each to respective lags, thereby converting from 
+        Iterates over predictor columns in `df` pivoting each to respective lags, thereby converting from
         long-to-wide for only a select number of lags.
-        
+
         :param df: pd.DataFrame, long-form dataset to be reshaped to wide form.
         :return: wide-form pd.DataFrame
         """
-        assert self.lag_length > 0, "Error! `self.lag_length` must be greater than 0!"
-        
         wide_format = pd.DataFrame()
-        
+
         # minimum number of observations per unit
-        # note - adding lags > min_group_obs will result in pointless features (all NaNs, which are filled with bfill)
-        min_group_obs = df.groupby(self.unit_identifier)[self.time_identifier].count().min()
+        min_group_obs = df.groupby(self.unit_identifier)[df.columns[0]].count().min()
         if min_group_obs < self.lag_length:
             logging.warning(f"Warning! Minimum number of observations per '{self.unit_identifier}' is {min_group_obs}, which is less than `self.lag_length` of {self.lag_length}. Lags will be constructed up to {min_group_obs} periods.")
-        
+
         # Identify invariant cols by `self.unit_identifier` - we don't want to add lags, these are fixed attributes
         uniques = df.reset_index().groupby(self.unit_identifier).apply(lambda x: x.nunique())
         invariant_cols = uniques.columns[(uniques == 1).all()].tolist()
@@ -190,7 +196,7 @@ class DataProcessor:
         cols = [i for i in df.columns if i not in [self.unit_identifier, self.target_feature]+invariant_cols]
         for col in cols:
             concat_list = []
-            for i in reversed(range(1, min(min_group_obs, self.lag_length+1))):
+            for i in reversed(range(1, min(min_group_obs+1, self.lag_length+1))):
                 concat_list.append(df.groupby(self.unit_identifier)[col].shift(i).rename(f'{col}_l{i}'))
 
             wide_col = pd.concat(concat_list, axis=1)
@@ -199,17 +205,18 @@ class DataProcessor:
                 axis=1)  # backfill any lags, in case df.groupby(self.unit_identifier)[col].shift(i).rename(f'{col}_l{i}') < lag_length
 
             wide_format = pd.concat([wide_format, wide_col], axis=1)  # concat with other columns
-        
+
         # Concat back invariant cols
         wide_format = pd.concat([wide_format, df[invariant_cols]], axis=1)
 
         return wide_format
-    
+
     def sample_group_observations(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Stratified sampling of observations for a given unit (`self.unit_identifier`) via quantiles of time. This is preferred over 
-        a simple random sample per unit, which may not be representative of the full time spectrum that the unit contains 
-        observations for. This function calculates the quantiles of `self.time_identifier`, where the number of 
-        quantiles (bins) is determined by min(`self.sampling_n`, total unit observations).
+        """Stratified sampling of observations for a given unit (`self.unit_identifier`) via quantiles of time. This is preferred over
+        a simple random sample per unit, which may not be representative of the full time spectrum that the unit contains
+        observations for. This function calculates the quantiles of `self.time_identifier`, where the number of
+        quantiles (bins) is determined by min(min(20, self.sampling_n), len(x)). The maximum number of observations per `unit_identifier` is
+        5 so that frequently-occurring `unit_identifiers`s don't dominate the loss.
 
         Args:
             df (pd.DataFrame): dataframe of all units and their timepoints, and all other columns
@@ -217,8 +224,12 @@ class DataProcessor:
         Returns:
             pd.DataFrame: sampled dataframe
         """
-        quantile = df.groupby([self.unit_identifier])[self.time_identifier].apply(lambda x:  pd.qcut(x, q=min(self.sampling_n, len(x)), labels=False)).reset_index(drop=True)
-        
+        min_obs_group = df.groupby([self.unit_identifier])[self.time_identifier].count().min()
+        if min_obs_group < self.sampling_n:
+            logging.warning(f"Warning! Minimum number of observations per '{self.unit_identifier}' is {min_obs_group}, which is less than `self.sampling_n` of {self.sampling_n}. This will yield an unbalanced panel. Either reduce the number of `self.sampling_n` to {min_obs_group} or verify that an unbalanced panel is acceptable.")
+
+        quantile = df.groupby([self.unit_identifier])[self.time_identifier].apply(lambda x:  pd.qcut(x, q=min(min(20, self.sampling_n), len(x)), labels=False)).reset_index(drop=True)
+
         return df.groupby([self.unit_identifier, quantile]).apply(lambda x: x.sample(1, random_state=self.seed)).reset_index(drop=True)
 
 
--- main.py
+++ main.py
@@ -22,49 +22,49 @@ def main(cfg: DictConfig) -> None:
     cfg : DictConfig
         An omegaconf object that stores the hydra config.
     """
-    
+
     # 1. Print parsed config
     logging.info(OmegaConf.to_yaml(cfg))
 
     # 2. Read and preprocess data
     df = pd.read_csv(cfg.data_path, compression='infer')
-    
+
     # 3. Initialize data processor class
     dp = DataProcessor(
         unit_identifier = cfg.unit_identifier,
         time_identifier  = cfg.time_identifier,
         target_feature = cfg.target_feature,
+        categorical_features_prefix = cfg.categorical_features_prefix,
         test_size = cfg.test_size,
         seed = cfg.seed,
-        model = cfg.model,
         lag_length = cfg.lag_length,
         sampling_n = cfg.sampling_n,
         oversample = cfg.oversample
     )
-    
+
     # 5. Preprocess data
-    df = dp.preprocess(df)
-    
+    df, y_lower_bound, y_upper_bound = dp.preprocess(df)
+
     # 6. Split data
-    #data = dp.split_transform(df)  # Tuple(X_train, X_test, y_train, y_test)
-    
+    data = dp.split_transform(df)  # Tuple(X_train, X_test, y_train, y_test)
+
     # 7. Instantiate appropriate model class
     # TODO
-        
+
     # # 8. Fit model
     # model.fit()
-    
+
     # # 9. Evaluate model
     # metrics = model.evaluate(uno=True, federated=False, brier=True if cfg.model in ['cox', 'gbm'] else False)
     # logging.info(f"Performance metrics: \n {metrics}")
     # with open(os.path.join(HydraConfig.get().runtime.output_dir, 'metrics.json'), 'w') as j:
     #     json.dump(metrics, j)
-    
+
     # # 10. Save weights for federated learning [optional]
     # if cfg.restrict_to_subset and cfg.model in ['svm', 'cox']:  # only save weights for SVM and Cox and if restricted to subset
     #     logging.info("Saving model weights for federated learning to directory './model_weights'.")
     #     dump(model.model, os.path.join('model_weights', f'{cfg.model}.joblib'))
-        
+
     # # 11. Save performance metrics and plot thereof
     # model.plot_forecast(model_type=cfg.model,
     #                     target_feature=cfg.target_feature,
@@ -72,5 +72,5 @@ def main(cfg: DictConfig) -> None:
     #                     output_path=HydraConfig.get().runtime.output_dir)
 
 if __name__ == '__main__':
-    
+
     main() # pylint: disable=E1120
