456c48ae002402fbf11f26f490934dce251e34fd
Joseph King <jking@luxoft.com>
2025-03-31 09:08:14 +0200

    added c-index IPCW evaluation of train and test sets in non-hyperopt routine

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+from sksurv.metrics import concordance_index_ipcw
+
 from utils import set_seeds, plot_losses
 from data_processing import DataProcessor
 
@@ -67,9 +69,10 @@ def main(cfg: DictConfig) -> None:
         y_upper_bound = y_upper_bound.where(df[cfg.target_feature] == 1, +np.inf)
 
     # Separate target feature from X matrix
-    df = df.set_index(cfg.unit_identifier)
+    df = df.set_index(cfg.unit_identifier)  # note - this has no effect on positional indexing below
     target_features = [cfg.time_identifier, cfg.target_feature]
     target = df[target_features].copy()
+    target[cfg.target_feature] = target[cfg.target_feature].astype(bool)
     X = df.drop(columns=target_features)
 
 
@@ -83,11 +86,18 @@ def main(cfg: DictConfig) -> None:
                             label_lower_bound=y_lower_bound[train_idx],
                             label_upper_bound=y_upper_bound[train_idx],
                             enable_categorical=True)
+        ytrain = target.iloc[train_idx].set_index(cfg.target_feature).to_records()
 
         dvalid = xgb.DMatrix(X.iloc[val_idx],
                     label_lower_bound=y_lower_bound[val_idx],
                     label_upper_bound=y_upper_bound[val_idx],
                     enable_categorical=True)
+
+        dtest = xgb.DMatrix(X.iloc[test_idx],
+            label_lower_bound=y_lower_bound[test_idx],
+            label_upper_bound=y_upper_bound[test_idx],
+            enable_categorical=True)
+        ytest = target.iloc[test_idx].set_index(cfg.target_feature).to_records()
 
         # Set default parameters
         params = {
@@ -95,7 +105,7 @@ def main(cfg: DictConfig) -> None:
             'objective': 'survival:aft',
             'eval_metric': 'aft-nloglik',
             'tree_method': 'hist',
-            'learning_rate': 0.03,
+            'learning_rate': 0.05,
             'aft_loss_distribution': 'normal',
             'aft_loss_distribution_scale': 1.0,
             'max_depth': 5,
@@ -108,7 +118,7 @@ def main(cfg: DictConfig) -> None:
         evals_result = {}
         bst = xgb.train(params,
                         dtrain,
-                        num_boost_round=10000,
+                        num_boost_round=10_000,
                         evals=[(dtrain, 'train'), (dvalid, 'valid')],
                         early_stopping_rounds=50,
                         evals_result=evals_result)
@@ -117,6 +127,22 @@ def main(cfg: DictConfig) -> None:
 
         # Plot the training and validation losses
         plot_losses(evals_result, HydraConfig.get().runtime.output_dir)
+
+        # Estimated median survival time, we treat as risk score, where higher risk scores indicate higher risk of failure
+        pred_test = -bst.predict(dtest)
+        pred_train = -bst.predict(dtrain)
+
+        # Calculate IPCW Concordance Index
+        c_indices = {}
+        c_indices['train'] = concordance_index_ipcw(survival_train=ytrain, survival_test=ytrain,
+                                              estimate=pred_train, tau=target.iloc[train_idx][cfg.time_identifier].max())[0]
+        c_indices['test'] = concordance_index_ipcw(survival_train=ytrain, survival_test=ytest,
+                                              estimate=pred_test, tau=target.iloc[train_idx][cfg.time_identifier].max())[0]
+
+        logging.info("Concordance Index (train): %.4f", c_indices['train'])
+        logging.info("Concordance Index (test): %.4f", c_indices['test'])
+
+
 
 
 if __name__ == '__main__':
